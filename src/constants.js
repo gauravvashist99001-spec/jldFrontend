f1724ccd778cd3c33784083895e6b49fac4b8aeb
GauravVashist239247 <gvashist1010@gmail.com>
2026-08-16 23:00:53 +0530

    name aaaaa

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 export const CARDS = [
-  { id: "card1", label: "Haryana" },
-  { id: "card2", label: "Faridabad" },
-  { id: "card3", label: "Gurgaon" },
-  { id: "card4", label: "Noida" },
+  { id: "card1", label: "Cd Haryana" },
+  { id: "card2", label: "Cd Faridabad" },
+  { id: "card3", label: "Cd Gurgaon" },
+  { id: "card4", label: "Cd Noida" },
 ];
