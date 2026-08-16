8a1bd22ec97dca19e8685d1e50a99c850b9e12ab
GauravVashist239247 <gvashist1010@gmail.com>
2026-08-16 21:17:36 +0530

    name as

--- src/main.jsx
+++ src/main.jsx
@@ -8,12 +8,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
-      <div id="warningdiv">
-        <h1 id="warning">
-          Purchase of lottery using this website is strictly prohabited in the state where lotteries are banned.
-          You must be above 18 Years to play online lottery
-        </h1>
-      </div>
+
     </BrowserRouter>
   </React.StrictMode>
 );
